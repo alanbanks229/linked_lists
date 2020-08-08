00a59ad742461bc9fb3818be213811884001aa9c
alanbanks229 <alanbanks229@gmail.com>
2020-08-08 19:07:24 -0400

    created print list methods and append to tail method, still debugging

--- singly_linked_list.rb
+++ singly_linked_list.rb
@@ -7,11 +7,41 @@ class Node
 end
 
 class LinkedList
-    
-    def initialize
+    def initialize()
         @head = nil;
     end
 
     def append_to_tail(value)
+        current_node = @head
+
+        # at this step current node is the first node
+        if current_node == nil
+            current_node = Node.new(value)
+            @head = current_node
+        else
+            while (current_node.next_node != nil)
+                current_node = current_node.next_node
+            end
+            current_node.value = value;
+        end
+    end
+
+    def print_list()
+        elements = []
+        current_node = @head
+        while current_node
+            elements.push(current_node)
+            current_node = current_node.next_node
+            puts "added node"
+        end
+        elements.each_with_index do |item, index|
+            puts "Node #{index}: Value = #{item.value} || next_node = #{item.next_node}"
+        end
     end
 end
+
+linkedlist1 = LinkedList.new()
+linkedlist1.append_to_tail(1)
+linkedlist1.append_to_tail(2)
+linkedlist1.append_to_tail(3)
+linkedlist1.print_list
