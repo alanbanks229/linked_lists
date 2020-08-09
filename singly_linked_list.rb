19ab758187074cd8aba011a26a0459079866bed8
alanbanks229 <alanbanks229@gmail.com>
2020-08-08 20:28:06 -0400

    refactored code, better use of initialization for linkedlist

--- singly_linked_list.rb
+++ singly_linked_list.rb
@@ -7,28 +7,20 @@ class Node
 end
 
 class LinkedList
-    def initialize()
-        @head = nil;
+    def initialize(value)
+        @head = Node.new(value)
     end
 
     def append_to_tail(value)
         current_node = @head
-
-        # at this step this instance of the linked list currently
-        # does not have a head node, so we add #{value} as head
-        if current_node == nil
-            current_node = Node.new(value)
-            @head = current_node
-        else
-            # while the current node iteration has a next node
-            while (current_node.next_node != nil)
-                current_node = current_node.next_node
-            end
-
-            # hitting this line below means we've reached a point where
-            # the current node's pointer is null... we are now giving it a value
-            current_node.next_node = Node.new(value)
+        # while the current node iteration has a next node
+        while (current_node.next_node != nil)
+            current_node = current_node.next_node
         end
+
+        # hitting this line below means we've reached a point where
+        # the current node's pointer is null... we are now giving it a value
+        current_node.next_node = Node.new(value)
     end
 
     def delete_value(value)
@@ -73,10 +65,9 @@ class LinkedList
 
 end
 
-linkedlist1 = LinkedList.new()
-linkedlist1.append_to_tail(1)
+linkedlist1 = LinkedList.new(1)
 linkedlist1.append_to_tail(2)
 linkedlist1.append_to_tail(3)
 linkedlist1.append_to_tail(4)
-linkedlist1.delete_value(4)
+linkedlist1.delete_value(2)
 linkedlist1.print_list
